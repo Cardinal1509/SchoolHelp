881078b53d6fcc0107d02f2864a04b0fc33d7fa0
Cardinal1509 <judeluesse@gmail.com>
2023-06-22 22:41:35 -0500

    1.0.1 Commit
    Second Ever Release of SchoolHelp is here!

--- SchoolHelp.py
+++ SchoolHelp.py
@@ -4,7 +4,31 @@ import webbrowser
 import time
 import pyautogui
 
-keyboard.add_hotkey("ctrl+alt+s", lambda: exec(open("./schoology.py").read()))
-keyboard.add_hotkey("ctrl+alt+c", lambda: exec(open("./clever.py").read()))
-keyboard.add_hotkey("ctrl+alt+t", lambda: exec(open("./tigerview.py").read()))
+def schoology():
+  webbrowser.open('https://ecusd7.schoology.com/login/ldap?&school=1126349310')
+  time.sleep(3)
+  pyautogui.moveTo(775, 645)
+  pyautogui.click()
+
+def clever():
+  webbrowser.open('https://clever.com/in/ecusd7')
+  time.sleep(3)
+  pyautogui.moveTo(800, 540)
+  pyautogui.click()
+  time.sleep(2)
+  pyautogui.moveTo(1300, 565)
+  pyautogui.click()
+
+def tigerview():
+  webbrowser.open('https://tigerview.ecusd7.org/')
+  time.sleep(3)
+  pyautogui.moveTo(950, 840)
+  pyautogui.scroll(-1000)
+  time.sleep(.5)
+  pyautogui.moveTo(950, 840)
+  pyautogui.click()
+
+keyboard.add_hotkey("ctrl+alt+s", lambda: schoology())
+keyboard.add_hotkey("ctrl+alt+c", lambda: clever())
+keyboard.add_hotkey("ctrl+alt+t", lambda: tigerview())
 keyboard.wait('esc') 
--- schoology.py
+++
@@ -1,9 +0,0 @@
-import webbrowser
-import time
-import pyautogui, sys
-def schoology():
-  webbrowser.open('https://ecusd7.schoology.com/login/ldap?&school=1126349310')
-  time.sleep(3)
-  pyautogui.moveTo(775, 645)
-  pyautogui.click()
-schoology()
